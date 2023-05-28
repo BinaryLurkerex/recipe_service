1eb2f0727e8b8d43893d2b0518ec2c4faa56920c
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-05-28 15:09:43 +0300

    fix: move icon to right pos

--- lib/src/presentation/widgets/recipe_card.dart
+++ lib/src/presentation/widgets/recipe_card.dart
@@ -15,27 +15,27 @@ class RecipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double blur = active! ? 16 : 0;
-    final double offset = active! ? 4 : 0;
-    final double top = active! ? 0 : 42;
-    final double left = active! ? 32 : 0;
+    final double blur = active! ? 16.0 : 0.0;
+    final double offset = active! ? 4.0 : 0.0;
+    final double top = active! ? 0.0 : 42.0;
+    final double left = active! ? 32.0 : 0.0;
 
     return AnimatedContainer(
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeOutQuint,
       margin: EdgeInsets.only(
         top: top,
-        right: 16,
         left: left,
+        right: 16.0,
       ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32),
+        borderRadius: BorderRadius.circular(32.0),
         color: Theme.of(context).colorScheme.secondary,
         boxShadow: [
           BoxShadow(
             color: Colors.black87.withOpacity(0.1),
             blurRadius: blur,
-            offset: Offset(0, offset),
+            offset: Offset(0.0, offset),
           )
         ],
       ),
@@ -53,7 +53,7 @@ class RecipeCard extends StatelessWidget {
           ),
           Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(32),
+              borderRadius: BorderRadius.circular(32.0),
               gradient: LinearGradient(
                 colors: [
                   Theme.of(context).colorScheme.secondary,
@@ -67,9 +67,7 @@ class RecipeCard extends StatelessWidget {
             ),
           ),
           Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+            bottom: 0.0,
             child: Container(
               height: 82.0,
               padding: const EdgeInsets.all(
@@ -78,15 +76,15 @@ class RecipeCard extends StatelessWidget {
               decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.secondary,
                   borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(32),
-                    bottomRight: Radius.circular(32),
+                    bottomLeft: Radius.circular(32.0),
+                    bottomRight: Radius.circular(32.0),
                   )),
               child: Text(
                 recipe!.name,
                 overflow: TextOverflow.ellipsis,
                 maxLines: 2,
                 style: const TextStyle(
-                  fontSize: 18,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
@@ -97,37 +95,41 @@ class RecipeCard extends StatelessWidget {
             bottom: 82.0,
             child: Padding(
               padding: const EdgeInsets.symmetric(
-                horizontal: 16,
+                horizontal: 16.0,
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8.5,
-                      vertical: 5,
-                    ),
-                    height: 24,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.white,
-                    ),
-                    child: Text(
-                      'Recipe',
-                      style: TextStyle(
-                        fontSize: 13,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 4.0,
+                ),
+                height: 24.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                ),
+                child: Text(
+                  'Recipe',
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  const Icon(
-                    Icons.bookmark_outline_rounded,
-                    color: Colors.white,
-                  ),
-                ],
+                ),
               ),
             ),
-          )
+          ),
+          const Positioned(
+            bottom: 82.0,
+            right: 0.0,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              child: Icon(
+                Icons.bookmark_outline_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
