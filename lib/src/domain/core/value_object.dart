6d4acdb2fd967cff451518ff600b898928ff17e0
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-05-30 08:16:44 +0300

    feat: add value object & validator

---
+++ lib/src/domain/core/value_object.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:recipe/src/domain/core/value_failure.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  String toString() => 'ValueObject($value)';
+
+  @override
+  bool operator ==(covariant ValueObject other) {
+    if (identical(this, other)) return true;
+
+    return other is ValueObject<T> && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+}
