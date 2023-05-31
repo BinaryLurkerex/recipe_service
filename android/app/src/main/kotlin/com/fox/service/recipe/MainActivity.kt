2d75285243ce73bf2e552f74308b6a76de1fc007
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-05-31 18:35:42 +0300

    feat: update google services configuration

--- android/app/src/main/kotlin/com/fox/service/recipe/MainActivity.kt
+++ android/app/src/main/kotlin/com/fox/service/recipe/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.recipe_service
+package com.fox.service.recipe
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    //! FLUTTER ENTRY POINT
 }
