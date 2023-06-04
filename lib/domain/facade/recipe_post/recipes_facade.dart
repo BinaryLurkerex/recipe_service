9a4aec437f74f932b86c09300e7c4985f01d26dc
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-04 14:22:42 +0300

    feat: add firebase facade for recipes

--- lib/domain/facade/recipe_post/recipes_facade.dart
+++ lib/domain/facade/recipe_post/recipes_facade.dart
@@ -4,7 +4,7 @@ import 'package:recipe_service/domain/core/value_object.dart';
 import 'package:recipe_service/domain/facade/recipe_post/post.dart';
 import 'package:recipe_service/domain/facade/recipe_post/recipes_failure.dart';
 
-abstract class PostFacade {
+abstract class RecipesFacade {
   Future<Either<RecipesFailure, KtList<Post>>> getPopularPosts([int offset]);
   Future<Either<RecipesFailure, KtList<Post>>> getUserBookmarkedPosts(UniqueId uuid, [int offset]);
   Future<Either<RecipesFailure, KtList<Post>>> getUserPosts(UniqueId uuid, [int offset]);
