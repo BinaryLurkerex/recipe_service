acea46318ffe7db66f48f544157bac7c7909ebf5
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-05 22:26:26 +0300

    feat: inject hive

--- lib/infrastucture/core/injector.dart
+++ lib/infrastucture/core/injector.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:recipe_service/infrastucture/local/entities/comment.dart';
+import 'package:recipe_service/infrastucture/local/entities/post.dart';
+import 'package:recipe_service/infrastucture/local/entities/profile.dart';
 
-// Generated injector
 import 'injector.config.dart';
 
 final getIt = GetIt.instance;
@@ -10,6 +13,11 @@ final getIt = GetIt.instance;
 @InjectableInit()
 Future<void> configureDependencies(String env) async {
   await Firebase.initializeApp();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(CommentModelAdapter());
+  Hive.registerAdapter(PostModelAdapter());
+  Hive.registerAdapter(ProfileModelAdapter());
 
   getIt.init(environment: env);
 }
