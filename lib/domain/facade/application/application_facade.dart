4fd2601620e262b20b066d7fb59a00704eeb393a
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-05 23:30:42 +0300

    feat: update application facade

--- lib/domain/facade/application/application_facade.dart
+++ lib/domain/facade/application/application_facade.dart
@@ -9,7 +9,7 @@ abstract class ApplicationFacade {
   Future<Either<ApplicationFailure, KtList<Post>>> getPopularPosts([int offset]);
   Future<Either<ApplicationFailure, KtList<Post>>> getUserBookmarkedPosts(UniqueId uuid, [int offset]);
   Future<Either<ApplicationFailure, KtList<Post>>> getUserPosts(UniqueId uuid, [int offset]);
-  Future<Either<ApplicationFailure, KtList<Post>>> uploadPost(Post post);
+  Future<Either<ApplicationFailure, Unit>> uploadPost(UniqueId uid, Post post);
   Future<Either<ApplicationFailure, Unit>> deletePost(UniqueId uid, UniqueId post);
   Future<Either<ApplicationFailure, Unit>> bookmarkPost(UniqueId uid, UniqueId post);
   Future<Either<ApplicationFailure, Unit>> likePost(UniqueId uid, UniqueId post);
--- lib/infrastucture/local/local_application_facade.dart
+++ lib/infrastucture/local/local_application_facade.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dartz/dartz.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
@@ -112,32 +114,159 @@ class LocalApplicationFacade implements ApplicationFacade {
   }
 
   @override
-  Future<Either<ApplicationFailure, KtList<Post>>> getPopularPosts([int? offset]) {
-    // TODO: implement getPopularPosts
-    throw UnimplementedError();
+  Future<Either<ApplicationFailure, KtList<Post>>> getPopularPosts([int? offset]) async {
+    try {
+      //! SOLVE FOREIGN SORT
+
+      return right(const KtList.empty());
+    } catch (_) {
+      return left(const ApplicationFailure.insufficientPermissions());
+    }
   }
 
   @override
-  Future<Either<ApplicationFailure, KtList<Post>>> getUserBookmarkedPosts(UniqueId uuid, [int? offset]) {
-    // TODO: implement getUserBookmarkedPosts
-    throw UnimplementedError();
+  Future<Either<ApplicationFailure, KtList<Post>>> getUserBookmarkedPosts(UniqueId uuid, [int? offset]) async {
+    try {
+      final userId = uuid.value.getOrElse(() => throw const ApplicationFailure.missingParametrs());
+      final userBookmarks = profileBox.get(userId)?.bookmarks;
+
+      if (userBookmarks?.isEmpty ?? false) {
+        return right(const KtList.empty());
+      }
+
+      if (offset == null) {
+        return right(await _getPostsWithID(userBookmarks!.toImmutableList()));
+      }
+
+      //TODO: implement offset slice
+      return left(const ApplicationFailure.serverError());
+    } catch (_) {
+      return left(const ApplicationFailure.insufficientPermissions());
+    }
   }
 
   @override
-  Future<Either<ApplicationFailure, KtList<Post>>> getUserPosts(UniqueId uuid, [int? offset]) {
-    // TODO: implement getUserPosts
-    throw UnimplementedError();
+  Future<Either<ApplicationFailure, KtList<Post>>> getUserPosts(UniqueId uuid, [int? offset]) async {
+    try {
+      final userId = uuid.value.getOrElse(() => throw const ApplicationFailure.missingParametrs());
+      final userPosts = postBox.values.where((element) => element.publisher == userId);
+
+      if (userPosts.isEmpty) {
+        return right(const KtList.empty());
+      }
+
+      if (offset == null) {
+        return right(
+          userPosts.map(
+            (post) {
+              return Post(
+                id: UniqueId.fromUniqueString(post.id),
+                owner: UniqueId.fromUniqueString(post.publisher),
+                recipe: Recipe(
+                  id: UniqueId.fromUniqueString(post.id),
+                  title: PostTitle(post.title),
+                  recipeImages: post.images.toImmutableList().mapNotNull((_) => null),
+                  recipeSubtitles: post.data.toImmutableList().mapNotNull((_) => null),
+                  recipeDatas: post.data.toImmutableList().mapNotNull((_) => null),
+                ),
+                likes: const KtList.empty(),
+                followers: const KtList.empty(),
+                bookmarks: const KtList.empty(),
+              );
+            },
+          ).toImmutableList(),
+        );
+      }
+
+      //TODO: implement offset slice
+      return left(const ApplicationFailure.serverError());
+    } catch (_) {
+      return left(const ApplicationFailure.insufficientPermissions());
+    }
   }
 
   @override
-  Future<Either<ApplicationFailure, Unit>> likePost(UniqueId uid, UniqueId post) {
-    // TODO: implement likePost
-    throw UnimplementedError();
+  Future<Either<ApplicationFailure, Unit>> likePost(UniqueId uid, UniqueId post) async {
+    try {
+      ProfileModel? model = profileBox.get(uid);
+
+      if (model == null) {
+        return left(const ApplicationFailure.missingPost());
+      }
+
+      profileBox.put(
+        uid,
+        ProfileModel(
+          id: model.id,
+          name: model.name,
+          avatar: model.avatar,
+          likes: model.likes +
+              [
+                post.value.getOrElse(
+                  () => throw const ApplicationFailure.missingParametrs(),
+                ),
+              ],
+          bookmarks: model.bookmarks,
+          following: model.following,
+        ),
+      );
+
+      return right(unit);
+    } catch (_) {
+      return left(
+        const ApplicationFailure.insufficientPermissions(),
+      );
+    }
   }
 
   @override
-  Future<Either<ApplicationFailure, KtList<Post>>> uploadPost(Post post) {
-    // TODO: implement uploadPost
-    throw UnimplementedError();
+  Future<Either<ApplicationFailure, Unit>> uploadPost(UniqueId uid, Post post) async {
+    try {
+      postBox.put(
+        post.id,
+        PostModel(
+          id: post.id.value.getOrElse(() => throw const ApplicationFailure.missingParametrs()),
+          title: post.recipe.title.value.getOrElse(() => throw const ApplicationFailure.missingParametrs()),
+          publisher: post.recipe.title.value.getOrElse(() => throw const ApplicationFailure.missingParametrs()),
+          date: post.recipe.title.value.getOrElse(() => throw const ApplicationFailure.missingParametrs()),
+          pages: 0,
+          images: const [],
+          data: const [],
+        ),
+      );
+
+      return right(unit);
+    } catch (_) {
+      return left(const ApplicationFailure.insufficientPermissions());
+    }
+  }
+
+  FutureOr<KtList<Post>> _getPostsWithID(KtList<String> ids) async {
+    final KtList<Post?> posts = ids.map<Post?>(
+      (id) {
+        final post = postBox.get(id);
+
+        if (post == null) {
+          return null;
+        }
+
+        return Post(
+          id: UniqueId.fromUniqueString(post.id),
+          owner: UniqueId.fromUniqueString(post.publisher),
+          recipe: Recipe(
+            id: UniqueId.fromUniqueString(post.id),
+            title: PostTitle(post.title),
+            recipeImages: post.images.toImmutableList().mapNotNull((_) => null),
+            recipeSubtitles: post.data.toImmutableList().mapNotNull((_) => null),
+            recipeDatas: post.data.toImmutableList().mapNotNull((_) => null),
+          ),
+          likes: const KtList.empty(),
+          followers: const KtList.empty(),
+          bookmarks: const KtList.empty(),
+        );
+      },
+    );
+
+    return posts.mapNotNull<Post>((_) => null);
   }
 }
